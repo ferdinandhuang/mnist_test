430f517c0fff0f54035f3eb921826a94c5a162a4
Ferdinand Huang <FerdinandHuang@hotmail.com>
2018-06-26 10:28:29 +0800

    loadminist

--- Linear_Regression/manual.py
+++ Linear_Regression/manual.py
@@ -1,3 +1,13 @@
-import loadminist
+import sys
 
-a,b,c,d = loadminist.load_mnist()
+sys.path.append('../')
+
+from loadminist import load
+
+# load data
+train_images, train_lables, test_images, test_lables = load('../minist').load_minist()
+print('train_images shape:%s' % train_images.shape)
+print('train_lables shape:%s' % train_lables.shape)
+print('test_images shape:%s' % test_images.shape)
+print('test_lables shape:%s' % test_lables.shape)
+
--- loadminist.py
+++ loadminist.py
@@ -2,8 +2,13 @@ import numpy as np
 import os
 import struct
 
-class load():
-    def load_mnist(path = 'minist'):
+
+class load:
+    def __init__(self,
+                 path='minist'):
+        self.path = path
+
+    def load_minist(self):
         """Read train and test dataset and lables from path"""
 
         train_image_path = 'train-images.idx3-ubyte'
@@ -12,34 +17,40 @@ class load():
         test_image_path = 't10k-images.idx3-ubyte'
         test_lable_path = 't10k-labels.idx1-ubyte'
 
-        with open(os.path.join(path, train_lable_path), 'rb') as labelpath:
+        with open(os.path.join(self.path, train_lable_path), 'rb') as labelpath:
             magic, n = struct.unpack('>II',
                                      labelpath.read(8))
             labels = np.fromfile(labelpath,
                                  dtype=np.uint8)
             train_lables = labels
 
-        with open(os.path.join(path, train_image_path), 'rb') as imgpath:
+        with open(os.path.join(self.path, train_image_path), 'rb') as imgpath:
             magic, num, rows, cols = struct.unpack('>IIII',
                                                    imgpath.read(16))
             images = np.fromfile(imgpath,
                                  dtype=np.uint8)
             train_images = images
 
-        with open(os.path.join(path, test_lable_path), 'rb') as labelpath:
+        with open(os.path.join(self.path, test_lable_path), 'rb') as labelpath:
             magic, n = struct.unpack('>II',
                                      labelpath.read(8))
             labels = np.fromfile(labelpath,
                                  dtype=np.uint8)
             test_lables = labels
 
-        with open(os.path.join(path, test_image_path), 'rb') as imgpath:
+        with open(os.path.join(self.path, test_image_path), 'rb') as imgpath:
             magic, num, rows, cols = struct.unpack('>IIII',
                                                    imgpath.read(16))
             images = np.fromfile(imgpath,
                                  dtype=np.uint8)
             test_images = images
 
-        return train_images,train_lables,test_images,test_lables
+        return train_images, train_lables, test_images, test_lables
 
 
+if __name__ == '__main__':
+    train_images, train_lables, test_images, test_lables = load().load_minist()
+    print('train_images shape:%s' % train_images.shape)
+    print('train_lables shape:%s' % train_lables.shape)
+    print('test_images shape:%s' % test_images.shape)
+    print('test_lables shape:%s' % test_lables.shape)
