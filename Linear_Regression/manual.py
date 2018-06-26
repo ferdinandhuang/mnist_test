811a5108a6103a0dbee2e8edc78637a7bb4c7570
Ferdinand Huang <FerdinandHuang@hotmail.com>
2018-06-26 16:18:02 +0800

    fix load

--- Linear_Regression/manual.py
+++ Linear_Regression/manual.py
@@ -1,13 +1,93 @@
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
 sys.path.append('../')
 
 from loadmnist import load
 
 # load data
-train_images, train_lables, test_images, test_lables = load('../mnist').load_mnist()
-print('train_images shape:%s' % train_images.shape)
-print('train_lables shape:%s' % train_lables.shape)
-print('test_images shape:%s' % test_images.shape)
-print('test_lables shape:%s' % test_lables.shape)
+train_images, train_labels, test_images, test_labels = load('../mnist').load_mnist()
+print('train_images shape:%s' % str(train_images.shape))
+print('train_labels shape:%s' % str(train_labels.shape))
+print('test_images shape:%s' % str(test_images.shape))
+print('test_labels shape:%s' % str(test_labels.shape))
 
+
+def layer_size(X, Y):
+    """
+    Get number of input and output size, and set hidden layer size
+    :param X: input dataset's shape(m, 784)
+    :param Y: input labels's shape(m,1)
+    :return:
+    n_x -- the size of the input layer
+    n_h -- the size of the hidden layer
+    n_y -- the size of the output layer
+    """
+
+    n_x = X.T.shape[0]
+    n_h = 4
+    n_y = Y.T.shape[0]
+
+    return n_x, n_h, n_y
+
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Initialize parameters
+    :param n_x: the size of the input layer
+    :param n_h: the size of the hidden layer
+    :param n_y: the size of the output layer
+    :return: dictionary of parameters
+    """
+
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2
+                  }
+
+    return parameters
+
+
+def forward_propagation(X, parameters):
+    """
+    Compute the forword propagation
+    :param X: input data (m, n_x)
+    :param parameters: parameters from initialize_parameters
+    :return:
+        cache: caches of forword result
+        A2: sigmoid output
+    """
+
+    X = X.T
+
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = 1 / (1 + np.exp(-Z2))
+
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+
+    return A2, cache
+
+X = train_images
+n_x, n_h, n_y = layer_size(X, train_labels)
+
+parameters = initialize_parameters(n_x, n_h, n_y)
+
+A2, cache = forward_propagation(X, parameters)
+print(A2)
--- loadmnist.py
+++ loadmnist.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import struct
+import matplotlib.pyplot as plt
 
 
 class load:
@@ -9,48 +10,90 @@ class load:
         self.path = path
 
     def load_mnist(self):
-        """Read train and test dataset and lables from path"""
+        """Read train and test dataset and labels from path"""
 
         train_image_path = 'train-images.idx3-ubyte'
-        train_lable_path = 'train-labels.idx1-ubyte'
+        train_label_path = 'train-labels.idx1-ubyte'
 
         test_image_path = 't10k-images.idx3-ubyte'
-        test_lable_path = 't10k-labels.idx1-ubyte'
+        test_label_path = 't10k-labels.idx1-ubyte'
 
-        with open(os.path.join(self.path, train_lable_path), 'rb') as labelpath:
-            magic, n = struct.unpack('>II',
-                                     labelpath.read(8))
-            labels = np.fromfile(labelpath,
-                                 dtype=np.uint8)
-            train_lables = labels
+        with open(os.path.join(self.path, train_label_path), 'rb') as labelpath:
+            magic, n = struct.unpack('>II', labelpath.read(8))
+            labels = np.fromfile(labelpath, dtype=np.uint8)
+            train_labels = labels.reshape(len(labels), 1)
 
         with open(os.path.join(self.path, train_image_path), 'rb') as imgpath:
-            magic, num, rows, cols = struct.unpack('>IIII',
-                                                   imgpath.read(16))
+            magic, num, rows, cols = struct.unpack('>IIII', imgpath.read(16))
             images = np.fromfile(imgpath,
-                                 dtype=np.uint8)
+                                 dtype=np.uint8).reshape(len(train_labels), 784)
             train_images = images
 
-        with open(os.path.join(self.path, test_lable_path), 'rb') as labelpath:
-            magic, n = struct.unpack('>II',
-                                     labelpath.read(8))
+        with open(os.path.join(self.path, test_label_path), 'rb') as labelpath:
+            magic, n = struct.unpack('>II', labelpath.read(8))
             labels = np.fromfile(labelpath,
                                  dtype=np.uint8)
-            test_lables = labels
+            test_labels = labels.reshape(len(labels), 1)
 
         with open(os.path.join(self.path, test_image_path), 'rb') as imgpath:
-            magic, num, rows, cols = struct.unpack('>IIII',
-                                                   imgpath.read(16))
-            images = np.fromfile(imgpath,
-                                 dtype=np.uint8)
+            magic, num, rows, cols = struct.unpack('>IIII', imgpath.read(16))
+            images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(test_labels), 784)
             test_images = images
 
-        return train_images, train_lables, test_images, test_lables
+        return train_images, train_labels, test_images, test_labels
 
 
 if __name__ == '__main__':
-    train_images, train_lables, test_images, test_lables = load().load_mnist()
-    print('train_images shape:%s' % train_images.shape)
-    print('train_lables shape:%s' % train_lables.shape)
-    print('test_images shape:%s' % test_images.shape)
-    print('test_lables shape:%s' % test_lables.shape)
+    train_images, train_labels, test_images, test_labels = load().load_mnist()
+    print('train_images shape:%s' % str(train_images.shape))
+    print('train_labels shape:%s' % str(train_labels.shape))
+    print('test_images shape:%s' % str(test_images.shape))
+    print('test_labels shape:%s' % str(test_labels.shape))
+
+    np.random.seed(1024)
+
+    trainImage = np.random.randint(60000, size=4)
+    testImage = np.random.randint(10000, size=2)
+
+    img1 = train_images[trainImage[0]].reshape(28, 28)
+    label1 = train_labels[trainImage[0]]
+    img2 = train_images[trainImage[1]].reshape(28, 28)
+    label2 = train_labels[trainImage[1]]
+    img3 = train_images[trainImage[2]].reshape(28, 28)
+    label3 = train_labels[trainImage[2]]
+    img4 = train_images[trainImage[3]].reshape(28, 28)
+    label4 = train_labels[trainImage[3]]
+
+    img5 = test_images[testImage[0]].reshape(28, 28)
+    label5 = test_labels[testImage[0]]
+    img6 = test_images[testImage[1]].reshape(28, 28)
+    label6 = test_labels[testImage[1]]
+
+
+    plt.figure(num='mnist', figsize=(2, 3))
+
+    plt.subplot(2, 3, 1)
+    plt.title(label1)
+    plt.imshow(img1)
+
+    plt.subplot(2, 3, 2)
+    plt.title(label2)
+    plt.imshow(img2)
+
+    plt.subplot(2, 3, 3)
+    plt.title(label3)
+    plt.imshow(img3)
+
+    plt.subplot(2, 3, 4)
+    plt.title(label4)
+    plt.imshow(img4)
+
+    plt.subplot(2, 3, 5)
+    plt.title(label5)
+    plt.imshow(img5)
+
+    plt.subplot(2, 3, 6)
+    plt.title(label6)
+    plt.imshow(img6)
+    plt.show()
+
